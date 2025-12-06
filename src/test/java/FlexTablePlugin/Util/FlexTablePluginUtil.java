11beb2c5c153ae702e743166ed088cbe407ad1e3
mr-tanzim-ahmed <mr.tanzim.ahmed@gmail.com>
2025-12-06 21:27:54 +0600

    Test Report

--- src/test/java/FlexTablePlugin/Util/FlexTablePluginUtil.java
+++ src/test/java/FlexTablePlugin/Util/FlexTablePluginUtil.java
@@ -2,6 +2,6 @@ package FlexTablePlugin.Util;
 
 public class FlexTablePluginUtil {
 
-    public final static int WAIT_TIME = 10; //Seconds
+    public final static int WAIT_TIME = 20; //Seconds
 }
 
